3ece5c1e19969417ded7c0ee356338e5b9c10b6f
Rusimka Dineva <rusimka.dineva@scalefocus.com>
2023-09-26 15:20:46 +0200

    - Made the H2 persistent - Migration from H2 database to MySql Database - Added sql script for inserting initial data in the application.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,28 @@
+-- Insert users
+INSERT INTO users (username, password, display_name) VALUES
+                                                         ('user1', 'password1', 'User One'),
+                                                         ('user2', 'password2', 'User Two');
+
+-- Insert blog posts
+INSERT INTO blog_post (blog_post_title, blog_post_text, user_id) VALUES
+                                                                     ('First Blog Post', 'This is the first blog post.', 1),
+                                                                     ('Second Blog Post', 'Another blog post here.', 1),
+                                                                     ('Third Blog Post', 'A blog post from another user.', 2);
+
+-- Insert tags
+INSERT INTO tags (tag_name) VALUES
+                                ('Technology'),
+                                ('Travel'),
+                                ('Food');
+
+-- Insert blog post tags (many-to-many relationship)
+INSERT INTO blog_post_tags (blog_post_id, tag_id) VALUES
+                                                      (1, 1),  -- First Blog Post is tagged with Technology
+                                                      (1, 2),  -- First Blog Post is tagged with Travel
+                                                      (2, 1),  -- Second Blog Post is tagged with Technology
+                                                      (3, 3);  -- Third Blog Post is tagged with Food
+
+
+
+
+
